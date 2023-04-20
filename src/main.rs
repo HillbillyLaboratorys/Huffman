7046748c25376cc6d63f046c6aea11e3a8cc656d
HillbillyLaboratorys <hardcasegar@gmail.com>
2023-04-19 22:22:30 -0500

    functional tree generation?

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,12 @@ fn main() {
 
     for i in input.chars() {
 
-        if frequency.contains_key(&i) {
-            current = frequency.get(&i).unwrap();
-            frequency.insert(i, current + 1);
+        if frequency.contains_key(&i.to_string()) {
+            current = frequency.get(&i.to_string()).unwrap();
+            frequency.insert(i.to_string(), current + 1);
         }
         else {
-            frequency.insert(i, 1);
+            frequency.insert(i.to_string(), 1);
         }
     }
     println!("{:?}", frequency);
@@ -48,21 +48,65 @@ fn main() {
 
     // sort by frequecy
 
-    let mut sorting: Vec<(char, i32)> = Vec::new();
+    // let mut sorting: Vec<(char, i32)> = Vec::new();
 
-    for (keys, values) in &frequency {
-        sorting.push((*keys, *values));
+    // for (keys, values) in &frequency {
+    //     sorting.push((*keys, *values));
+    // }
+
+    // sorting.sort_by(|a,b| b.1.cmp(&a.1));
+    // println!("{:?}", sorting);
+
+    let mut node_list: Vec<Node> = Vec::new();
+
+    // create tree
+
+    for (i, j) in &frequency {
+        node_list.push(Node::new(i, *j));
     }
 
-    sorting.sort_by(|a,b| b.1.cmp(&a.1));
-    println!("{:?}", sorting);
+    let mut left: Node;
+    let mut right: Node;
+    let mut sum: i32;
 
-    // create look up table
-    let mut table = HashMap::new();
+    while node_list.len() > 1 {
+        node_list.sort_by(|a,b| a.freq.cmp(&b.freq));
 
-    for (i, v) in sorting.iter().enumerate() {
-        table.insert(v.0, i);
+        left = node_list.pop().unwrap();
+        right = node_list.pop().unwrap();
+        sum = left.freq + right.freq;
+
+        node_list.push(Node::new("", sum).left(left).right(right));
     }
-    println!("{:?}", table);
 
+    // // create look up table
+    // let mut table = HashMap::new();
+
+    // for (i, v) in sorting.iter().enumerate() {
+    //     table.insert(v.0, i);
+    // }
+    // println!("{:?}", table);
+
+}
+pub struct Node<'a> {
+    symbol: &'a str,
+    freq: i32,
+    l: Option<Box<Node<'a>>>,
+    r: Option<Box<Node<'a>>>,
+}
+
+impl<'a> Node<'a> {
+    pub fn new(symbol: &'a str, freq: i32) -> Self {
+        Node { symbol: (symbol), freq: (freq), l: (None), r: (None) }
+    }
+
+    pub fn left(mut self, node: Node<'a>) -> Self {
+        self.l = Some(Box::new(node));
+        self
+    }
+
+    pub fn right(mut self, node: Node<'a>) -> Self {
+        self.r = Some(Box::new(node));
+        self
+    }
 }
