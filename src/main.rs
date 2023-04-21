29c64a9faf4933b2ea32f7e08cad814bc1ca75f4
HillbillyLaboratorys <hardcasegar@gmail.com>
2023-04-20 19:43:09 -0500

    functional

--- src/main.rs
+++ src/main.rs
@@ -27,39 +27,11 @@ fn main() {
             frequency.insert(i.to_string(), 1);
         }
     }
-    println!("{:?}", frequency);
-
-    // sort by frequecy
-    // let mut sorted = Vec::new();
-
-    // for (_keys, values) in &frequency {
-    //     sorted.push(values);
-    // }
-    // sorted.sort();
-    // println!("{:?}", sorted);
-
-    // // create lookup table with huffman codes
-    // let mut table = HashMap::new();
-
-    // for (keys, values) in &frequency {
-    //     table.insert( keys, sorted.binary_search(&values).expect("miss match of frequency"));
-    // }
-    // println!("{:?}", table);
-
-    // sort by frequecy
-
-    // let mut sorting: Vec<(char, i32)> = Vec::new();
-
-    // for (keys, values) in &frequency {
-    //     sorting.push((*keys, *values));
-    // }
-
-    // sorting.sort_by(|a,b| b.1.cmp(&a.1));
-    // println!("{:?}", sorting);
-
-    let mut node_list: Vec<Node> = Vec::new();
+    //println!("{:?}", frequency);
 
     // create tree
+
+    let mut node_list: Vec<Node> = Vec::new();
 
     for (i, j) in &frequency {
         node_list.push(Node::new(i.to_string(), *j));
@@ -84,22 +56,38 @@ fn main() {
 
     let mut code: i64 = 0;
 
-    fn recuse_tree (node: &Node, code: &i64, table: &mut HashMap<String,i64>) {
-        if !node.l.is_none() { 
-            recuse_tree(&*node.l?, &(code << 1), table);
-        }
-        if !node.r.is_none() {
-            recuse_tree(&*node.r.unwrap(), &((code << 1) + 1), table);
-        }
-        
-        table.insert(node.symbol, *code);
-        return;
-        
-    }
-    
-    recuse_tree(&node_list[0], &code, &mut table);
+    node_list[0].codes_recursive(&mut code, &mut table);
 
     //println!("{:?}", table);
+
+    //create output file
+    let mut file = match File::create("C:\\work\\Huffman\\encoded.txt") {
+        Err(why) => panic!("couldn't create file: {}", why),
+        Ok(file) => file,
+    };
+
+    let mut output: String = String::new();
+
+    // put codes in output 
+    for (i, j) in &table {
+        output.push_str(&i);
+        output.push(' ');
+        output.push_str(&j.to_string());
+        output.push(' ');
+    }
+    output.push('\n');
+    
+    // encode input and add to output
+    for i in input.chars() {
+        output.push_str(table.get(&i.to_string()).unwrap().to_string().as_ref());
+        output.push(' ');
+    }
+
+    // write table to file
+    match file.write_all(output.as_bytes()) {
+        Err(why) => panic!("couldn't write to file: {}", why),
+        Ok(_) => println!("successfully wrote to file"),
+    }
 
 }
 pub struct Node {
@@ -122,5 +110,25 @@ impl<'a> Node {
     pub fn right(mut self, node: Node) -> Self {
         self.r = Some(Box::new(node));
         self
+    }
+    pub fn codes_recursive (&mut self, code: &mut i64, table: &mut HashMap<String,i64>) {
+        match self.l {
+            Some(ref mut node) => {
+                *code = *code << 1;
+                node.codes_recursive(code, table)
+            }
+            None => (),
+        }
+        match self.r {
+            Some(ref mut node) => {
+                *code = (*code << 1) + 1;
+                node.codes_recursive(code, table)
+            }
+            None => (),
+        }
+        if self.symbol != "" {
+            table.insert(self.symbol.clone(), *code);
+        }
+        return;
     }
 }
